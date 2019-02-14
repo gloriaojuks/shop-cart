8079d200f0df6e5a9cdc29d8085cce9e9f29db0e
Gloria Ojukwu <gloriao@phreetech.com>
2019-02-14 17:37:07 +0100

    introducing states and props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 import App from "./App.js"
-import "./style.css";
+import "./style.css"
 
 
 let destination = document.querySelector("#root");
