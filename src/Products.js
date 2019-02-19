e274244239f0833fe57cf14d9c783d0d2981c33e
Gloria Ojukwu <gloriao@phreetech.com>
2019-02-19 14:07:17 +0100

    slight changes on the header

--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 export class Products extends Component {
   constructor(props) {
     super(props);
@@ -13,27 +14,54 @@ export class Products extends Component {
     this.setState({ qty: this.state.qty + 1 });
   }
 
+  remove() {
+    if(this.state.qty < 1){
+    this.setState({qty: 0})
+    }else{
+    this.setState({ qty: this.state.qty - 1 });
+  }
+}
   render() {
     return (
       <div>
+        <img height="100" width="100" src={this.props.imgUrl} alt=""/>
         <p className="contain">{this.props.name}- ${this.props.price}</p>
-        
-        {/* <hr /> */}
 
+        {/* <hr /> */}
+        <p>(Add to Cart)</p>
         <button
           style={{
             backgroundColor: "tomato",
-            color: "#464e54",
+            color: "#fff",
             borderRadius: "5px",
-            width: "100px",
-            height: "50px",
+            width: "30px",
+            height: "30px",
             fontWeight: "800",
-            fontSize: "13px"
+            fontSize: "13px",
+            margin: "2px"
+
           }}
           onClick={this.buy.bind(this)}
           className="contain"
         >
-          Add to Cart
+          +
+        </button>
+
+        <button
+          style={{
+            backgroundColor: "tomato",
+            color: "#fff",
+            borderRadius: "5px",
+            width: "30px",
+            height: "30px",
+            fontWeight: "800",
+            fontSize: "13px",
+            margin: "2px"
+          }}
+          onClick={this.remove.bind(this)}
+          className="contain"
+        >
+          -
         </button>
         <h3 className="contain">qty: {this.state.qty} item(s)</h3>
         <p style={{textAlign: "right", color: "#f4f4f4",marginTop: "-20px"}}>Enjoy your shoping</p>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App.js"
+import Main from "./Main.js"
 import Header from "./Header.js"
+import Footer from "./Footer.js"
 import "./style.css"
 
 
@@ -10,7 +11,8 @@ let destination = document.querySelector("#root");
 ReactDOM.render(
     <div>
         <Header />
-        <App />
+        <Main />
+        <Footer />
     </div>,
     destination
 )
