932db464e1af849b33171a0ea743188f56466a84
gloria ojuks <gloriaojuks@gmail.com>
2019-02-21 15:46:51 +0100

    contact change

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <React.Fragment>
     <div style={{textAlign: "center", backgroundColor: "#ccc", width: "100%", maxWidth: "80%", margin: "0 auto", height: "500px", padding: "80px"}}>
-         <h1>Contact</h1>
+         <h1>Contact Us Today</h1>
          <p>contact me on giftdfgr8@gmail.com</p>
     </div>
 </React.Fragment>
