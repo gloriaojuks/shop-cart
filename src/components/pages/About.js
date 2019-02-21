f4a9f5cb9b9c71eae7aa9a371c013f950f15f81c
Gloria Ojukwu <gloriao@phreetech.com>
2019-02-21 14:38:16 +0100

    slight changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
    <React.Fragment>
-       <div style={{textAlign: "center"}}>
+       <div style={{textAlign: "center", backgroundColor: "#ccc", width: "100%", maxWidth: "80%", margin: "0 auto", height: "500px", padding: "80px"}}>
             <h1>About</h1>
             <p>This is a simple shopping cart system built with React</p>
        </div>
